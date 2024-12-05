a2c8ffdc23d22165dc6c1f8166817c58572a6971
CoderDebajyoti <debajyoti.475@gmail.com>
2024-12-05 20:15:47 +0530

    update script

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
 (function() {
-    emailjs.init("YOUR_USER_ID"); // Replace with your EmailJS user ID
+    emailjs.init("6c6xqC3eBlUYXOnRA"); // Replace with your EmailJS user ID
 })();
 
 function sendMail() {
     const templateParams = {
         name: document.querySelector('input[name="full-name"]').value,
         email: document.querySelector('input[name="email"]').value,
-        description: document.querySelector('textarea[name="description"]').value,
+        description: document.querySelector('textarea[name="message"]').value,
     };
 
-    emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', templateParams)
+    emailjs.send('service_a0zw6u5', 'template_1s1jcf6', templateParams)
         .then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
             alert('Your message has been sent successfully!');
